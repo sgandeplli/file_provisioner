6a4e37c135cb5f43861768e6c936a8001c4f6746
sgandeplli <sgandepalli@softility.com>
2024-10-09 12:43:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,13 @@ resource "google_compute_instance" "default" {
  name = "tsunade"
  machine_type = "e2-medium"
 
- boot_disk {
-   initialization_params {
-       image = "centos-cloud/centos-stream-9"
-      }
+ # Boot disk image for CentOS 9
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-9"
+    }
   }
+
 
   network_interface {
     network = "default"
@@ -21,13 +23,13 @@ resource "google_compute_instance" "default" {
   }
 
 provisioner "file" {
-  source = "/home/hr377/sample.sh"
+  source = "/home/hr376/sample.sh"
   destination = "/tmp/shell_script.sh"
 
   connection {
     type = "ssh"
     user = "hr376"
-    private_key = file("/home/hr76/.ssh/is_rsa")
+    private_key = file("/home/hr376/.ssh/is_rsa")
     host        = self.network_interface[0].access_config[0].nat_ip
   }
 }
