d9b9f03d67e88698353c2e434c571530c59d7ebb
sgandeplli <sgandepalli@softility.com>
2024-10-11 12:19:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,16 +29,12 @@ provisioner "file" {
   connection {
     type = "ssh"
     user = "hr376"
-    private_key = var.private_key_path
+    private_key = file("/home/hr376/.ssh/id_rsa")
     host        = self.network_interface[0].access_config[0].nat_ip
   }
 }
 
 }
 
-variable "private_key_path" {
-  description = "Path to the private SSH key"
-  type        = string
-  default     = file("/home/hr376/.ssh/id_rsa")  
-}
+
 
